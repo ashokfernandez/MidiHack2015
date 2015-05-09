d2656cd01af6fc84c379edceca7844c890702074
Ashok Fernandez <ashok.fernandez@gmail.com>
2015-05-10 01:29:45 +0200

    Audio Engine can sequence hits in order

--- MidiHack2015/AudioEngine.m
+++ MidiHack2015/AudioEngine.m
@@ -6,47 +6,49 @@
 //  Copyright (c) 2015 Ashok Fernandez. All rights reserved.
 //
 
+@import Foundation;
 #import "AudioEngine.h"
 #import "AudioSamplePlayer.h"
 #import <TheAmazingAudioEngine/TheAmazingAudioEngine.h>
 
+#define NUM_CHANNELS 3
+#define NUM_STEPS 4
+
 @interface AudioEngine ()
 @property (nonatomic) AEAudioController *audioController;
-@property     AudioSamplePlayer *track1;
-//@property (nonatomic) NSArray *channels;
-//@property (nonatomic) AEAudioUnitFilter *pulseReverb;
-//@property (nonatomic) AEAudioUnitFilter *boopReverb;
-//@property (nonatomic) BOOL *channelActivity;
+@property (nonatomic) NSArray *channels;
+@property (nonatomic) NSTimer *timer;
+@property (nonatomic) NSUInteger step;
+@property (nonatomic) NSArray *pattern;
 @end
 
 @implementation AudioEngine
 
 -(BOOL)setup {
+
     self.audioController = [[AEAudioController alloc] initWithAudioDescription:[AEAudioController nonInterleavedFloatStereoAudioDescription]];
     
     // Initialise tracks
-    NSError *errorTrack1 = nil;
-    self.track1 =
-    [AudioSamplePlayer audioFilePlayerWithURL:
-     [[NSBundle mainBundle] URLForResource:@"synth" withExtension:@"caf"]
-                              audioController: self.audioController
-                                        error:&errorTrack1];
+    AudioSamplePlayer *track1 = [AudioSamplePlayer audioFilePlayerWithURL:[[NSBundle mainBundle] URLForResource:@"Kick" withExtension:@"caf"]
+                                            audioController: self.audioController
+                                                      error:NULL];
 
-//    NSError *errorTrack2 = nil;
-//    [AEAudioFilePlayer audioFilePlayerWithURL:
-//     [[NSBundle mainBundle] URLForResource:@"Track 2" withExtension:@"m4a"]
-//                              audioController: self.audioController
-//                                        error:&errorTrack2];
-    
-//    NSLog(@"%@", error);
-    
-    // Set to loop mode
-//    self.track1.loop = YES;
-//    track2.loop = YES;
-    
-    // Add channels
-    [self.audioController addChannels:[NSArray arrayWithObjects:self.track1, nil]];
+    AudioSamplePlayer *track2 = [AudioSamplePlayer audioFilePlayerWithURL:[[NSBundle mainBundle] URLForResource:@"Snare" withExtension:@"caf"]
+                                                          audioController: self.audioController
+                                                                    error:NULL];
 
+    AudioSamplePlayer *track3 = [AudioSamplePlayer audioFilePlayerWithURL:[[NSBundle mainBundle] URLForResource:@"Hat" withExtension:@"caf"]
+                                                          audioController: self.audioController
+                                                                    error:NULL];
+
+    
+    // Save channels to object
+    self.channels = [NSArray arrayWithObjects:track1, track2, track3, nil];
+    
+    // Add channels to main audio controller
+    [self.audioController addChannels: self.channels];
+
+    // Start playback
     NSError *error = nil;
     if ( ! [self.audioController start:&error])
     {
@@ -54,14 +56,45 @@
         return NO;
     }
     
+    // Initialise the step value and timer
+    self.step = 0;
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.3
+                                     target:self
+                                   selector:@selector(incrementStep)
+                                   userInfo:nil
+                                    repeats:YES];
+    
+    // Hardcode a pattern to play
+    NSArray *kickPattern = [NSArray arrayWithObjects: @1, @0, @0, @0, nil];
+    NSArray *snarePattern = [NSArray arrayWithObjects: @0, @0, @1, @0, nil];
+    NSArray *hatPattern = [NSArray arrayWithObjects: @0, @1, @1, @1, nil];
+    
+    self.pattern = [NSArray arrayWithObjects: kickPattern, snarePattern, hatPattern, nil];
+    
     return YES;
 
 }
 
-- (BOOL)start
-{
-    [self.track1 play];
-    return YES;
+-(void)incrementStep {
+
+    AudioSamplePlayer *channel;
+    NSArray *pattern;
+
+    for (int i=0; i<NUM_CHANNELS; i++) {
+        channel = self.channels[i];
+        pattern = self.pattern[i];
+        
+        if ([pattern[self.step] isEqualToNumber: @1]) {
+            [channel play];
+        }
+    }
+
+    self.step += 1;
+    if(self.step >= NUM_STEPS) {
+        self.step = 0;
+    }
 }
+
 
 @end
